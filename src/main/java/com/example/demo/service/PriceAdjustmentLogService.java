83578fe7d2b392fb46df1bf02a521370553fd90a
bala-amypo <bala@amypo.in>
2025-12-27 05:58:51 +0000

    User commit

--- src/main/java/com/example/demo/service/PriceAdjustmentLogService.java
+++ src/main/java/com/example/demo/service/PriceAdjustmentLogService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.model.PriceAdjustmentLog;
+
 import java.util.List;
 
 public interface PriceAdjustmentLogService {
-
-    PriceAdjustmentLog saveLog(PriceAdjustmentLog log);
-
-    List<PriceAdjustmentLog> getLogsByEventId(Long eventId);
+    
+    PriceAdjustmentLog createLog(PriceAdjustmentLog log);
+    
+    List<PriceAdjustmentLog> getAdjustmentsByEvent(Long eventId);
+    
+    List<PriceAdjustmentLog> getAllAdjustments();
 }

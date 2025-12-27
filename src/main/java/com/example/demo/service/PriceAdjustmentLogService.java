85e42707618371e3836104acfaea45b6d2f3beb1
bala-amypo <bala@amypo.in>
2025-12-27 05:06:26 +0000

    User commit

--- src/main/java/com/example/demo/service/PriceAdjustmentLogService.java
+++ src/main/java/com/example/demo/service/PriceAdjustmentLogService.java
@@ -1,14 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.model.PriceAdjustmentLog;
-
 import java.util.List;
 
 public interface PriceAdjustmentLogService {
-    
-    PriceAdjustmentLog createLog(PriceAdjustmentLog log);
-    
-    List<PriceAdjustmentLog> getAdjustmentsByEvent(Long eventId);
-    
-    List<PriceAdjustmentLog> getAllAdjustments();
+
+    PriceAdjustmentLog saveLog(PriceAdjustmentLog log);
+
+    List<PriceAdjustmentLog> getLogsByEventId(Long eventId);
 }
--- src/main/java/com/example/demo/service/PriceAdjustmentLogServiceImpl.java
+++ src/main/java/com/example/demo/service/PriceAdjustmentLogServiceImpl.java
@@ -1,35 +1,26 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.model.PriceAdjustmentLog;
 import com.example.demo.repository.PriceAdjustmentLogRepository;
-import com.example.demo.service.PriceAdjustmentLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class PriceAdjustmentLogServiceImpl implements PriceAdjustmentLogService {
-
-    private final PriceAdjustmentLogRepository priceAdjustmentLogRepository;
+public class PriceAdjustmentLogServiceImpl
+        implements PriceAdjustmentLogService {
 
     @Autowired
-    public PriceAdjustmentLogServiceImpl(PriceAdjustmentLogRepository priceAdjustmentLogRepository) {
-        this.priceAdjustmentLogRepository = priceAdjustmentLogRepository;
+    private PriceAdjustmentLogRepository repository;
+
+    @Override
+    public PriceAdjustmentLog saveLog(PriceAdjustmentLog log) {
+        return repository.save(log);
     }
 
     @Override
-    public PriceAdjustmentLog createLog(PriceAdjustmentLog log) {
-        return priceAdjustmentLogRepository.save(log);
-    }
-
-    @Override
-    public List<PriceAdjustmentLog> getAdjustmentsByEvent(Long eventId) {
-        return priceAdjustmentLogRepository.findByEventId(eventId);
-    }
-
-    @Override
-    public List<PriceAdjustmentLog> getAllAdjustments() {
-        return priceAdjustmentLogRepository.findAll();
+    public List<PriceAdjustmentLog> getLogsByEventId(Long eventId) {
+        return repository.findByEventId(eventId);
     }
 }
